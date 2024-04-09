a570021736c0fa26d8aabe95f0a6ca3399a92d31
antoniadelgado7789 <ma.antoniadelgado@cca.edu.co>
2024-04-09 10:53:25 -0500

    change

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
-const totalCards = 12;
-let cards = {};
-let selectedcards = {};
-let valuesUsed = {};
+const totalCards = 16;
+const availableCards = ['A', 'K', 'Q', 'J','D'];
+let cards = [];
+let selectedCards = [];
+let valuesUsed = [];
 let currentMove = 0;
+let currentAttempts = 0;
 
-let cardTemplate = '<div class="card"><divclass="back"></div><div class= "face"></div></div>';
+let cardTemplate = '<div class="card"><div class="back"></div><div class="face"></div></div>';
 
-function activate (e) {
-    if (currentMove < 2){
-        e.target.classlist.add('active');
-        if (! selectedcards[0] || selectedcards[0] !== e.tanget) {
-            selectedcards.push(target);
+function activate(e) {
+   if (currentMove < 2) {
+      
+      if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active') ) {
+         e.target.classList.add('active');
+         selectedCards.push(e.target);
 
-            if (++currentMove == 2) {
-                if (selectedcards[0].querySelectorAll('.face')[0].innerHTML == selectedcards[1].querySelectorAl('.face')[0].innerHTMLs) {
-                    selectedcards = [];
-                    currentMove = 0; 
-                }
-                else {
-                    setTimeout(() => {
-                       selectedcards[0].classlist.remove('active')
-                       selectedcards[1].classlist.remove('active');
-                       selectedcards = [];
-                       currentMove = 0;
-                    }, 600);
-                    
-                }
+         if (++currentMove == 2) {
+
+            currentAttempts++;
+            document.querySelector('#stats').innerHTML = currentAttempts + ' intentos';
+
+            if (selectedCards[0].querySelectorAll('.face')[0].innerHTML == selectedCards[1].querySelectorAll('.face')[0].innerHTML) {
+               selectedCards = [];
+               currentMove = 0;
             }
-        }
-    }
+            else {
+               setTimeout(() => {
+                  selectedCards[0].classList.remove('active');
+                  selectedCards[1].classList.remove('active');
+                  selectedCards = [];
+                  currentMove = 0;
+               }, 600);
+            }
+         }
+      }
+   }
 }
 
-function randomValue () {
-    let rnd = Math.floor[Math.random() * totalCards * 0.5];
-    let values = valuesUsed.filter(value => value === rnd);
-    if (values.length < 2) {
-        valuesUsed.push(rnd);
-    }
-    else {
-        randomValue();
-    }
+function randomValue() {
+   let rnd = Math.floor(Math.random() * totalCards * 0.5);
+   let values = valuesUsed.filter(value => value === rnd);
+   if (values.length < 2) {
+      valuesUsed.push(rnd);
+   }
+   else {
+      randomValue();
+   }
 }
 
-for (let i= 0; i < totalCards; i++) {
-    let div = document.createElement('div')
-    div.innerHTML = cardTemplate;
-    cards.push(div);
-    document.querySelector('#game').append(cards[i]);
-    randomValue();
-    cards[i].querySelectorAll('.face')[0].innerHTML =valuesUsed [i];
-    cards[i].querySelectorAll('.card')[0].addEventListener('click',activate);
+function getFaceValue(value) {
+   let rtn = value;
+   if (value < availableCards.length) {
+      rtn = availableCards[value];
+   }
+   return rtn;
+}
+
+for (let i=0; i < totalCards; i++) {
+   let div = document.createElement('div');
+   div.innerHTML = cardTemplate;
+   cards.push(div);
+   document.querySelector('#game').append(cards[i]);
+   randomValue();
+   cards[i].querySelectorAll('.face')[0].innerHTML = getFaceValue(valuesUsed[i]);
+   cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
 }
